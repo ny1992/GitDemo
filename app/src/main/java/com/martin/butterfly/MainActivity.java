100a7d15870e63ae349a9c018b7b99811f59d27c
ny <334907301@qq.com>
2016-09-09 21:16:46 +0800

    First commit

---
+++ app/src/main/java/com/martin/butterfly/MainActivity.java
@@ -0,0 +1,21 @@
+package com.martin.butterfly;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.martin.butterfly.master.ButterFlyDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showDialog();
+    }
+
+    private void showDialog() {
+        ButterFlyDialog butterFlyDialog = new ButterFlyDialog(this);
+        butterFlyDialog.show();
+    }
+}
